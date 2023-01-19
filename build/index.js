064802225ae00cae171dcec844b7a011ad5ad2f8
richard <rldarklight@gmail.com>
2023-01-19 20:03:21 +0800

    test

--- build/index.js
+++ build/index.js
@@ -1,18 +1,14 @@
 import { Octokit } from "octokit";
-import { data } from "./test.js";
 // TOKEN -- Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({
-    auth: `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`
+    auth: `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 `
 });
-// await octokit.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}', {
-//     owner: 'gitRenc',
-//     repo: 'test-gitAction',
-//     workflow_id: 'node CI'
-//   })
-const response = await octokit.request('GET /repos/gitRenc/test-gitAction/actions/workflows/main.yml');
-// const {
-//     data: { login },
-//   } = await octokit.rest.users.getAuthenticated();
-console.log(response);
-console.log("Hello, %s", data.name);
+const response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+    owner: 'gitRenc',
+    repo: 'test-gitAction',
+    workflow_id: 'main.yml',
+    ref: 'main'
+});
+// should be return 204
+console.log(response.status);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,17 @@ import { data } from "./test.js";
 
 // TOKEN -- Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({ 
-    auth: `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` 
+    auth: `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 ` 
 });
 
-// await octokit.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}', {
-//     owner: 'gitRenc',
-//     repo: 'test-gitAction',
-//     workflow_id: 'node CI'
-//   })
-// const response = await octokit.request('GET /repos/gitRenc/test-gitAction/actions/workflows/main.yml')
-// const {
-//     data: { login },
-//   } = await octokit.rest.users.getAuthenticated();
 
 const response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
     owner: 'gitRenc',
     repo: 'test-gitAction',
     workflow_id: 'main.yml',
-    ref: 'main',
-    
+    ref: 'main'
   })
 
-  console.log(response);
-//   console.log("Hello, %s", data.name);
+// should be return 204
+console.log(response.status);
 
