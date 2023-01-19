c07154ea09899d596637680768563d7abde681f7
richard <rldarklight@gmail.com>
2023-01-20 02:28:20 +0800

    FINISHED

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import { Octokit, App } from "octokit";
-import { data } from "./test.js";
+import { Octokit } from "octokit";
 
 // TOKEN -- Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({ 
     auth: `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 ` 
 });
-
-
+// DATA INFO
+// owner        : git repo owner
+// repo         : git repo
+// workflow_id  : workflow filename in .github/workflow 
+// ref          : branch
+//
 const response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
     owner: 'gitRenc',
     repo: 'test-gitAction',
