1b8d6ac662772a33fbd0488ffac0559e2a885d19
richard <rldarklight@gmail.com>
2023-01-19 17:14:08 +0800

    test2

--- build/index.js
+++ build/index.js
@@ -1,7 +1,18 @@
 import { Octokit } from "octokit";
 import { data } from "./test.js";
-const octokit = new Octokit({ auth: `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` });
-const { data: { login }, } = await octokit.rest.users.getAuthenticated();
-console.log("Hello, %s", login);
+// TOKEN -- Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({
+    auth: `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`
+});
+// await octokit.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}', {
+//     owner: 'gitRenc',
+//     repo: 'test-gitAction',
+//     workflow_id: 'node CI'
+//   })
+const response = await octokit.request('GET /repos/gitRenc/test-gitAction/actions/workflows/main.yml');
+// const {
+//     data: { login },
+//   } = await octokit.rest.users.getAuthenticated();
+console.log(response);
 console.log("Hello, %s", data.name);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import { Octokit, App } from "octokit";
 import { data } from "./test.js";
 
+// TOKEN -- Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({ 
+    auth: `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` 
+});
 
-const octokit = new Octokit({ auth: `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` });
-const {
-    data: { login },
-  } = await octokit.rest.users.getAuthenticated();
+// await octokit.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}', {
+//     owner: 'gitRenc',
+//     repo: 'test-gitAction',
+//     workflow_id: 'node CI'
+//   })
+// const response = await octokit.request('GET /repos/gitRenc/test-gitAction/actions/workflows/main.yml')
+// const {
+//     data: { login },
+//   } = await octokit.rest.users.getAuthenticated();
 
+const response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+    owner: 'gitRenc',
+    repo: 'test-gitAction',
+    workflow_id: 'main.yml',
+    ref: 'main',
+    
+  })
 
-  console.log("Hello, %s", login);
-  console.log("Hello, %s", data.name);
+  console.log(response);
+//   console.log("Hello, %s", data.name);
+
