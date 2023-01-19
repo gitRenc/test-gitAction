b98e92c5ba5907dfa43d21cb843c76d67c59e7ac
richard <rldarklight@gmail.com>
2023-01-20 02:14:21 +0800

    python requests

--- .github/workflows/python/api.py
+++ .github/workflows/python/api.py
@@ -1,33 +1,33 @@
+import os
 import requests
 
 import logging
-import os
-# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
 
-# WebClient instantiates a client that can call API methods
-# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-token=os.environ.get("SLACK_BOT_TOKEN")
+
+# PUT TOKEN ON GITHUB SECERETS
+token = os.environ.get("SLACK_BOT_TOKEN")
 if not token:
     raise RuntimeError("SLACK_BOT_TOKEN env var is not set!")
-print("env good!")
-client = WebClient()
-logger = logging.getLogger(__name__)
+print("env pass!")
 
 # ID of channel you want to post message to
+url = "https://slack.com/api/chat.postMessage"
+headers = {"Authorization": "Bearer {}".format(
+    token), "Content-type": "application/json"}
 channel_id = "C020XUK58T1"
+data = {
+    "channel": "C020XUK58T1",
+    "text": "Hello world :tada:"
+}
+
 
 try:
+    response = requests.post(url, headers=headers, json=data)
     # Call the conversations.list method using the WebClient
-    result = client.chat_postMessage(
-        channel=channel_id,
-        text="Pull Request"
-        # You could also use a blocks[] array to send richer content
-    )
-    # Print result, which includes information about the message (like TS)
-    print(result)
 
-except SlackApiError as e:
-    print(f"Error: {e}")
+    # Print result, which includes information about the message (like TS)
+    print(response)
+except:
+    print("api call failed")
+
 # C020XUK58T1
